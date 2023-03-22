a9c21606d2e0b2398dfdc42a87250858c2bf6a37
Danila <gurpsdanya@gmail.com>
2023-03-22 21:00:14 +0300

    Just trying few css styles

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
-import React, { Component } from 'react'
-import Table from './Table'
+import React, { Component } from 'react';
+import Header from './components';
+import './App.css';
 
 class App extends Component {
   render() {
-    const characters = [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ]
 
     return (
-      <div className="container">
-        <Table characterData={characters}/>
-      </div>
+    <>
+      <Header />
+    </>
     )
   }
 }
---
+++ src/components.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css'
+const Header = () => {
+  return (
+  <header className='page-header'>
+    <section className="sytename--box">
+        <img className="logo" src="img/D6Icon2_copy.png"></img>
+        <a className="sytename--link" href="#">What`s GURPS</a>
+    </section>
+  </header>
+  )
+}
+
+export default Header
