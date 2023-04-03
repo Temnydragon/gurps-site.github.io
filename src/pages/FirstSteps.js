1c742708bf1c0f0bdab298e20282d52690af5029
Danila <gurpsdanya@gmail.com>
2023-04-03 20:04:14 +0300

    pages FirstSteps and Advices

--- src/pages/FirstSteps.js
+++ src/pages/FirstSteps.js
@@ -4,6 +4,46 @@ import MainContent from '../Annotation';
 function FirstSteps () {
     return (
       <>
+        <main className='page-main'>
+          <section class="main-content--box topmargin">
+            <h1 className="text-style--title">Как начать играть в GURPS?</h1>
+            <p className='text-style--maintext main-content--text bordernone textlinebreak'>
+              {'Если вы ознокомились с информацией на данном сайте и хотите начать играть в GURPS, вам необходимо немного подготовиться:'}
+            </p>
+          </section>
+
+          <section className="main-content--box numberedlist">
+              <p className="text-style--maintext main-content--text textlinebreak bordernone notoppad">
+                {'1. Прочитайте сокращённый вариант общих правил под названием GURPS Lite. На русском языке данную книжку вы можете найти '}
+                <a  className="linkstyle--textlink" href="https://fuzionpowered.ucoz.ru/_ld/0/8_GURPS_lite_ru.pdf">здесь.</a>
+                {' Этого будет достаточно для понимания основ и подготовки к первой игре.'}
+              </p>
+
+              <p className="text-style--maintext main-content--text textlinebreak bordernone notoppad">
+                {'2. Как только вы изучите основы, можете поискать в сети и/или среди друзей более опытного игрока, который захочет быть “Мастером” в вашей партии так как в любой НРИ должен быть ведущий.'}
+              </p>
+
+              <p className="text-style--maintext main-content--text textlinebreak bordernone notoppad">
+                {'3. Создайте своего персонажа. Для этого вам понадобится шаблон листа персонажа, который вы можете найти '}
+                <a className='linkstyle--textlink' href="https://vk.com/wall-3656533_1322">здесь.</a>
+                {' Также, для этих целей вам могут быть полезны сайт '}
+                <a className='linkstyle--textlink' href="https://mentor.gurps.ru/">GURPS Mentor</a>
+                {' и специальная программа '}
+                <a className='linkstyle--textlink' href="https://gurpscharactersheet.com/">GURPS Character Sheet.</a>
+              </p>
+          </section>
+
+          <section class="main-content--box">
+            <p className='text-style--maintext bordernone textlinebreak'>
+              {'Когда вы сделаете всё вышеописанное, можете спокойно начинать свои приключения по мирам, созданным другими любителями GURPS. Если же вам захочется большего, вы всегда можете обратиться к многообразию книг на '}
+              <a className='linkstyle--textlink' href="https://www.sjgames.com/gurps/">официальном сайте системы</a>
+              {' или посетить другие разделы данного сайта.'}
+            </p>
+          </section>
+        </main>
+
+        <footer className="page-footer">
+        </footer>
       </>
     );
   };
--- src/pages/ForMasterAndPlayer.js
+++ src/pages/ForMasterAndPlayer.js
@@ -2,11 +2,39 @@ import React from 'react';
 
 const ForMasterAndPlayer = () => {
   return (
-    <div>
-      <h1>
-        Here will be advices and usefull URL.
-      </h1>
-    </div>
+    <>
+      <main>
+        <section class="main-content--box topmargin">
+          <h1 className="text-style--title">Советы по игре</h1>
+          <p className='text-style--maintext main-content--text bordernone textlinebreak'>
+            {'В данном разделе представлены советы игрокам и мастерам, которые уже имеют хотя бы минимальный опыт игры. Если вы только начинаете своё знакомство с GURPS,  сначала ознакомьтесь с информацией из раздела “С чего начать?”.'}
+            {'\n\nА теперь перейдем к советам:'}
+          </p>
+        </section>
+
+        <section className="main-content--box numberedlist">
+            <p className="text-style--maintext main-content--text textlinebreak bordernone notoppad">
+              {'1. По мере увеличения игрового опыта, вам рано или поздно станет мало тех возможностей, что даёт GURPS Lite. Как только это случится, попробуйте углубиться в полную версию правил. Для игрока достаточно изучить книгу “GURPS Basic Set: Characters”, которую можно приобрести в оригинале на '}
+              <a  className="linkstyle--textlink" href="https://www.sjgames.com/gurps/books/Basic/">официальном сайте</a>
+              {' или скачать русскую версию, например, '}
+              <a  className="linkstyle--textlink" href="https://www.rulit.me/data/programs/resources/pdf/StivDzhekson_GURPS-4E-BasicSet(polnyyperevod)_RuLit_Me_389900.pdf">отсюда.</a>
+            </p>
+
+            <p className="text-style--maintext main-content--text textlinebreak bordernone notoppad">
+              {'2. Если вы являетесь начинающим мастером и у вас мало опыта, помимо вышеупомянутой книги вам будет очень полезно хотя бы бегло ознакомится с “GURPS Basic Set: Campaigns”. В ней вы найдёте всё необходимое для прописывания игровых миров независимо от их сеттинга, основного жанра и степени правдоподобности. Данную книгу также можно найти на ранее указанном официальном сайте системы.'}
+            </p>
+
+            <p className="text-style--maintext main-content--text textlinebreak bordernone notoppad">
+              {'3. Также я считаю очень важным сказать всем игрокам и  мастерам следующее: всегда старайтесь заранее продумать и сказать другим, чего именно вы хотите от игры в целом, и что вы ожидаете от своих приключений как персонаж в мире мастера в частности!!'}
+              {'\nЭто крайне важно так как я лично несколько раз сталкивался с неприятными ситуациями, в которых я или другие игроки не могли реализовать свои желания, а мастер не понимал почему в его подробно прописанном мире вдруг всем становится не интересно.'}
+              {'\nТакие моменты обычно легко решаемы путём своевременного обсуждения желаний как игроков так и мастера ещё на этапе подготовки к игре, но многие новички об этом просто забывают.'}
+            </p>
+        </section>
+      </main>
+
+      <footer className="page-footer">
+      </footer>
+    </>
   );
 };
   
