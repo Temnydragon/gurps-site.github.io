61a03c0a61c60d8625fdb155e73151973c8ee894
Danila <gurpsdanya@gmail.com>
2023-05-09 00:14:58 +0300

    add better validation and modal description on distattack page

--- src/CalculatorComponents/DistAttackForm.js
+++ src/CalculatorComponents/DistAttackForm.js
@@ -5,7 +5,7 @@ class DistAttackForm extends Component {
     constructor(props) {
         super(props)
         this.state = { basikskill:10, distance:1, speed:0, size:0, accuracy:0, shots:1, zone:0, effectiveskill:10, modifierDistAndSpeed:'+0', modifierSize:'+0', modifierAccuracy:'+0', modifierShots:'+0', modifierZone:'0', diceRollResult:' - '}
-        this.handleChange = this.handleChange.bind(this)
+        this.handleBasicSkillChange = this.handleBasicSkillChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleDistAndSpeedChange = this.handleDistAndSpeedChange.bind(this)
         this.handleSizeChange = this.handleSizeChange.bind(this)
@@ -26,11 +26,10 @@ class DistAttackForm extends Component {
     }
 
     
-    handleChange(event){
+    handleBasicSkillChange(event){ 
       this.setState({
-        // Computed property names
-        // keys of the objects are computed dynamically
-        [event.target.name] : event.target.value
+        /*Проверка для устранения отрицательных значений*/
+        [event.target.name] : (event.target.value === '' ? '' : (Math.abs(event.target.value) >= 0 ? Math.abs(event.target.value) : 0))
       })
 
       this.setState(this.ComputEffectiveSkill)
@@ -179,11 +178,9 @@ class DistAttackForm extends Component {
     }
 
     handleDistAndSpeedChange(event){
-
       this.setState({
-        [event.target.name] : event.target.value
+        [event.target.name] : (event.target.value === '' ? '' : (Math.abs(event.target.value) >= 0 ? Math.abs(event.target.value) : 0))
       })
-
       this.setState(this.ComputeDistSpeedModifier)
       this.setState(this.ComputEffectiveSkill)
     }
@@ -200,7 +197,7 @@ class DistAttackForm extends Component {
     handleAccuracyChange(event){
 
       this.setState({
-        [event.target.name] : event.target.value
+        [event.target.name] : (event.target.value === '' ? '' : (Math.abs(event.target.value) >= 0 ? Math.abs(event.target.value) : 0))
       })
       this.setState(this.ComputAccuracyModifier)
       this.setState(this.ComputEffectiveSkill)
@@ -226,17 +223,68 @@ class DistAttackForm extends Component {
                     <a href="#closeModal" title="Close" className='text-style--closebutton'>×</a>
                   </div>
                   <div className='modal-maintext'>
-                    <p className='text-style--maintext bordernone textlinebreak'>
-                      {'Данный модуль ускоряет ряд расчётов дистанционных атак. \n\nВ поле "Базовое умение персонажа" следует вводить число, равное уровню умения персонажа во владении конкретным дистанционным оружием.'}
+                    <p className='text-style--modalparagraph'>
+                      {'Данный модуль ускоряет процесс расчётов дистанционных атак по правилам GURPS.'}
                     </p>
-                    <p className='text-style--maintext bordernone textlinebreak'>
-                      {'\nПоля ввода "Расстояние до цели" и "Скорость цели" используются для расчёта единого модификатора за скорость и расстояние согласно правилам и таблице на странице 550 в базовой книге правил. (Найти перевод книги можно '}
+                    <h3 className='text-style--modalparagraph'>
+                      {'Важно:'}
+                    </h3>
+                    <p className='text-style--modalparagraph'>
+                      {'Все поля данной формы (кроме "Модификатор размера цели" и "Эффективное умение персонажа") не могут содержать отрицательных чисел, знаков, или быть пустыми!'}
+                      {'\n\nВсе поля данной формы не должны содержать дробные значения согласно правилам системы (в случае ввода, дробная часть не будет учитываться в расчётах)!'}
+                      {'\n\nСогласно правилам системы 1 ярд = 1 метру при расчёте расстояний, скоростей и размеров (в реальности это соотношение примерно равно 0,91 к 1). Если вам очень важна точность, переведите метры в ярды и округлите вверх до целого числа перед вводом значения в форму.'}
+                    </p>
+                    <h3 className='text-style--modalparagraph'>
+                      {'Базовый уровень умения:'}
+                    </h3>
+                    <p className='text-style--modalparagraph'>
+                      {'В поле "Базовое умение персонажа" следует вводить число, равное базовому уровню умения персонажа во владении конкретным дистанционным оружием.\nБазовый уровень - это не модифицированный уровень умения, отражающий средние шансы на выполнение атаки без учёта ситуативных модификаторов.\nПодробнее про это можно почитать на странице 171 в базовой книге правил. (Найти перевод книги можно '}
                       <a  className="linkstyle--textlink" href="https://www.rulit.me/data/programs/resources/pdf/StivDzhekson_GURPS-4E-BasicSet(polnyyperevod)_RuLit_Me_389900.pdf">здесь</a>
                       {')'}
                     </p>
+                    <h3 className='text-style--modalparagraph'>
+                      {'Расстояние и скорость цели:'}
+                    </h3>
                     <p className='text-style--maintext bordernone textlinebreak'>
-                      {'\nУ любого дистанционного оружия (будь то простой арбалет или миниган) есть показатель "Точности". Это премия, которую ваш персонаж получает, если один или более раз использует манёвр "Прицеливание" перед атакой. \nЕсли вы целитесь больше одной секунды или используете дополнительные возможности (например, сошки для винтовки), то вы можете получить большую премию.'}
-                      {'\nПодробности о манёвре "Прицеливание" можно узнать на странице 364 в базовой книге.'}
+                      {'Поля ввода "Расстояние до цели" и "Скорость цели" используются для расчёта единого модификатора за скорость и расстояние согласно правилам и таблице на странице 550 в базовой книге правил. Значения этих полей не должны быть отрицательными.'}
+                    </p>
+                    <h3 className='text-style--modalparagraph'>
+                      {'Модификатор размера цели (МР):'}
+                    </h3>
+                    <p className='text-style--modalparagraph'>
+                      {'"Модификатор размера цели" отражает наиболее крупное измерение существа или предмета: длину, ширину или высоту. Большинство людей и близких по размеру к взрослому человеку объектов имеют 0-вой модификатор размера.'}
+                      {'\nЕсли ваш персонаж стреляет по меньшим или большим целям, он получает соответствуюйщий штраф или бонус к попаданию. Просто введите МР цели в соответствующее поле формы.'}
+                      {'\nЧтобы определить размер цели можно воспользоваться правилами и таблицей со страницы 19 в базовой книге правил (ссылка выше).'}
+                      {' Если же персонаж хочет попасть по транспорту, лучше обратиться к таблицам и книге, из которых вы планируете брать данный транспорт (на русском языке в подобных таблицах данный модификатор обозначен как "МР", а на английском - "SM")'}
+                    </p>
+                    <h3 className='text-style--modalparagraph'>
+                      {'Точность оружия:'}
+                    </h3>
+                    <p className='text-style--modalparagraph'>
+                      {'У любого дистанционного оружия (будь то простой арбалет или миниган) есть показатель "Точность". Это премия, которую ваш персонаж получает, если один или более раз использует манёвр "Прицеливание" перед атакой. Подробности о манёвре "Прицеливание" можно узнать на странице 364 в базовой книге правил.'}
+                      {'\nЕсли персонаж целится больше одной секунды или использует дополнительные возможности (например, сошки для винтовки), то он может получить большую премию. Просто введите сумму всех полученных таким образом бонусов в поле "Точность оружия" в форме.'}
+                    </p>
+                    <h3 className='text-style--modalparagraph'>
+                      {'Количество выстрелов:'}
+                    </h3>
+                    <p className='text-style--modalparagraph'>
+                      {'У любого стрелкового оружия есть показатель Скорострельности (сокращённо "Сс" на русском или "RoF" на английском). Если Сс составляет 1, то оружие может выстрелить только один раз за атаку. Если Сс 2 и больше, то из оружия можно вести "Стрельбу очередями" согласно правилам на странице 373.'}
+                      {'\nПри ведении стрельбы очередями персонаж получает бонус к эффективному уровню умения за большое количество выстрелов в рамках одной атаки.'}
+                      {'\nПросто введите значение, равное числу выстрелов из оружия за ход, в поле "Количество выстрелов" в форме.'}
+                    </p>
+                    <h3 className='text-style--modalparagraph'>
+                      {'Зоны попадания:'}
+                    </h3>
+                    <p className='text-style--modalparagraph'>
+                      {'Когда ваш персонаж атакует цель, которая является другим человеком или подобным существом, вы можете выбрать любую видимую вам зону попадания на теле этой цели. Если вы этого не указываете, по умолчанию считается, что вы целитесь в торс (+0 к попаданию).'}
+                      {'\nДля выбора конкретной зоны просто нажмите на один из возможных вариантов списка в соответствующей части формы. Указанный рядом с выбранной зоной модификатор автоматически изменит значение эффективного умения.'}
+                    </p>
+                    <h3 className='text-style--modalparagraph'>
+                      {'Эффективный уровень умения:'}
+                    </h3>
+                    <p className='text-style--modalparagraph'>
+                      {'Эффективный уровень умения персонажа отражает реальные шансы успешно совершить атаку с учётом всех применяемых модификаторов.\nЗначение поля "Эффективное умение персонажа" изменяется автоматически при изменении значений вычисляемых модификаторов и базового умения персонажа в форме.'}
+                      {'\nЕсли значение данного поля окажется меньше 3, при попытке провести "бросок успеха" будет выведено предупреждение о невозможности атаки. Это связано с броском трёх шестигранных кубиков и сравнением эффективного умения с выпавшей на кубиках суммой (подробности смотри на странице 343 базовой книги правил). Так как выпавшая сумма не может быть меньше трёх любой персонаж, чьё эффективное умение ниже данного значения, вообще не имеет шансов попасть этой атакой.'}
                     </p>
                   </div>
                 </div>
@@ -256,7 +304,7 @@ class DistAttackForm extends Component {
               <div className='borderedbottom-block'>
                 <div className="skill-box">
                   <label htmlFor="basikskill-id" className="text-style--maintext textblock-center">Базовое умение персонажа</label>
-                  <input className="text-style--maintext" type="number" name="basikskill" id="basikskill-id" value={this.state.basikskill} onChange={this.handleChange}/>
+                  <input className="text-style--maintext" type="number" name="basikskill" id="basikskill-id" value={this.state.basikskill} min='0' max='100' required onChange={this.handleBasicSkillChange}/>
                 </div>
               </div>
 
@@ -265,25 +313,25 @@ class DistAttackForm extends Component {
               <div className='borderedbottom-block'>
                 <div className="modifiers-box--double">
                   <label htmlFor="distance-id" className="text-style--maintext distance">Расстояние до цели (в ярдах)</label>
-                  <input className="text-style--maintext distcount" type="number" name="distance" id="distance-id" value={this.state.distance} min="0" onChange={this.handleDistAndSpeedChange}/>
+                  <input className="text-style--maintext distcount" type="number" name="distance" id="distance-id" value={this.state.distance} min="0" required onChange={this.handleDistAndSpeedChange}/>
                   <label htmlFor="speed-id" className="text-style--maintext speed">Скорость цели (ярд/с)</label>
-                  <input className="text-style--maintext speedcount" type="number" name="speed" id="speed-id" value={this.state.speed} min="0" onChange={this.handleDistAndSpeedChange}/>
+                  <input className="text-style--maintext speedcount" type="number" name="speed" id="speed-id" value={this.state.speed} min="0" required onChange={this.handleDistAndSpeedChange}/>
                   
                   <input className="text-style--maintext input-style--doublemodifier distspeedoutput" type="text" name="distspeedoutput" id="distspeedoutput-id" value={this.state.modifierDistAndSpeed} readOnly />
                 </div>
                 <div className="modifiers-box--single">
                   <label htmlFor="size-id" className="text-style--maintext">Модификатор размера цели</label>
-                  <input className="text-style--maintext" type="number" name="size" id="size-id" value={this.state.size} onChange={this.handleSizeChange}/>
+                  <input className="text-style--maintext" type="number" name="size" id="size-id" value={this.state.size} required onChange={this.handleSizeChange}/>
                   <input className="text-style--maintext input-style--singlemodifier" type="text" name="sizeoutput" id="sizeoutput-id" value={this.state.modifierSize} readOnly />
                 </div>
                 <div className="modifiers-box--single">
                   <label htmlFor="accuracy-id" className="text-style--maintext">Точность оружия</label>
-                  <input className="text-style--maintext" type="number" name="accuracy" id="accuracy-id" value={this.state.accuracy} min="0" onChange={this.handleAccuracyChange}/>
+                  <input className="text-style--maintext" type="number" name="accuracy" id="accuracy-id" value={this.state.accuracy} min="0" required onChange={this.handleAccuracyChange}/>
                   <input className="text-style--maintext input-style--singlemodifier" type="text" name="accuracyoutput" id="accuracyoutput-id" value={this.state.modifierAccuracy} readOnly />
                 </div>
                 <div className="modifiers-box--single">
                   <label htmlFor="shots-id" className="text-style--maintext">Количество выстрелов</label>
-                  <input className="text-style--maintext" type="number" name="shots" id="shots-id" value={this.state.shots} min="1" onChange={this.handleShotsChange}/>
+                  <input className="text-style--maintext" type="number" name="shots" id="shots-id" value={this.state.shots} min="1" required onChange={this.handleShotsChange}/>
                   <input className="text-style--maintext input-style--singlemodifier" type="text" name="shotsoutput" id="shotsoutput-id" value={this.state.modifierShots} readOnly />
                 </div>
               </div>
